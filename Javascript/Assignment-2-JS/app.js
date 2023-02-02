b4739a39c671904bff313ec8d0491a5f7af01525
timi-petre <gaxxis@yahoo.com>
2023-02-02 14:16:53 +0200

    `Javascript` - Assignment 2

---
+++ Javascript/Assignment-2-JS/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const op = '+';
+const a = +prompt('Primul numar: ', 3);
+const result = prompt('Operatia: ', op);
+const b = +prompt('Al doilea numar: ', 10);
+
+switch (result) {
+	case '+':
+		alert(`${a} + ${b} = ${a + b}`);
+		break;
+	case '-':
+		alert(`${a} - ${b} = ${a - b}`);
+		break;
+	case '*':
+		alert(`${a} * ${b} = ${a * b}`);
+		break;
+	case '*':
+		alert(`${a} / ${b} = ${a / b}`);
+		break;
+
+	default:
+		break;
+}
