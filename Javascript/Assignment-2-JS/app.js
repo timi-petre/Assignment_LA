79b0895208be73132edbdc7c0ca16bf4f30932c7
timi-petre <gaxxis@yahoo.com>
2023-02-03 20:31:03 +0200

    `Assignment 2` modified

--- Javascript/Assignment-2-JS/app.js
+++ Javascript/Assignment-2-JS/app.js
@@ -5,20 +5,34 @@ const a = +prompt('Primul numar: ', 3);
 const result = prompt('Operatia: ', op);
 const b = +prompt('Al doilea numar: ', 10);
 
-switch (result) {
-	case '+':
-		alert(`${a} + ${b} = ${a + b}`);
-		break;
-	case '-':
-		alert(`${a} - ${b} = ${a - b}`);
-		break;
-	case '*':
-		alert(`${a} * ${b} = ${a * b}`);
-		break;
-	case '/':
-		alert(`${a} / ${b} = ${a / b}`);
-		break;
+if (
+	typeof a === 'number' &&
+	!Number.isNaN(a) &&
+	typeof b === 'number' &&
+	!Number.isNaN(b)
+) {
+	switch (result) {
+		case '+':
+			alert(`${a} + ${b} = ${a + b}`);
+			break;
+		case '-':
+			alert(`${a} - ${b} = ${a - b}`);
+			break;
+		case '*':
+			alert(`${a} * ${b} = ${a * b}`);
+			break;
+		case '/':
+			if (a != 0 && b != 0) {
+				alert(`${a} / ${b} = ${a / b}`);
+			} else {
+				alert(`${a} / ${b} = 0`);
+			}
+			break;
 
-	default:
-		break;
+		default:
+			alert(`Te rog introdu date corecte!`);
+			break;
+	}
+} else {
+	alert(`Te rog introdu numai numere`);
 }
