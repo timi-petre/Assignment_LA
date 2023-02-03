fd82c6006237ebe9fdd176497a9d32d5bc0d572a
timi-petre <gaxxis@yahoo.com>
2023-02-03 20:37:26 +0200

    `Assignment 2` modified

--- Javascript/Assignment-2-JS/app.js
+++ Javascript/Assignment-2-JS/app.js
@@ -22,10 +22,12 @@ if (
 			alert(`${a} * ${b} = ${a * b}`);
 			break;
 		case '/':
-			if (a != 0 && b != 0) {
-				alert(`${a} / ${b} = ${a / b}`);
+			if (a == 0 && b == 0) {
+				alert(`${a} / ${b} = Result is undefined`);
+			} else if (b == 0) {
+				alert(`${a} / ${b} = Cannot divide by zero`);
 			} else {
-				alert(`${a} / ${b} = 0`);
+				alert(`${a} / ${b} = ${a / b}`);
 			}
 			break;
 
