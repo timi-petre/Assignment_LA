243f2decc28b8262d88717711bf71287a73a4ff0
timi-petre <gaxxis@yahoo.com>
2023-04-14 13:43:41 +0300

    Assignments

--- Javascript/Assignment-1-JS/app.js
+++ Javascript/Assignment-1-JS/app.js
@@ -1,12 +1,17 @@
-const volum = document.getElementById('volum');
+// const volum = document.getElementById('volum');
 
-const r = +prompt('Valoarea razei: ', 2);
-const H = +prompt('Valoarea inaltimii', 3);
-const PI = 3.14;
+// const r = +prompt('Valoarea razei: ', 2);
+// const H = +prompt('Valoarea inaltimii', 3);
+// const PI = 3.14;
 
-let result = r ** 2 * PI * H;
-volum.textContent = result;
+// let result = r ** 2 * PI * H;
+// volum.textContent = result;
 
-console.log(result);
-alert(`V = r^2 * π * H 
-Volumul este: ${result}`);
+// console.log(result);
+// alert(`V = r^2 * π * H
+// Volumul este: ${result}`);
+
+let a = +prompt('dsadsad');
+let b = +prompt('dsaddsdsad');
+console.log(typeof a);
+console.log('suma este : ', a + b);
