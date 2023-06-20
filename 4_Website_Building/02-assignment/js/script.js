57cb55538334d5a8b3bdcf36b1f88745b4834110
timi-petre <12413810+timi-petre@users.noreply.github.com>
2023-06-20 22:50:53 +0300

    Formular Javascript A2

--- 4_Website_Building/02-assignment/js/script.js
+++ 4_Website_Building/02-assignment/js/script.js
@@ -1,47 +1,96 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
+'use strict';
 
-const nameUser = 'new_user';
-const passwordUser = '123456789';
+window.addEventListener('load', () => {
+	const myForm = document.getElementById('myForm');
+	const nameUser = 'new_user';
+	const passwordUser = '1';
 
-var smallError = document.getElementById('small');
-smallError.textContent = '';
+	const username = document.getElementById('username');
+	const password = document.getElementById('password');
 
-function showError(input, message) {
-	const formControl = input.parentElement;
-	formControl.className = 'form-control error';
-	smallError.textContent = message;
-}
+	let message = document.getElementById('msg');
+	message.textContent = '';
+	let arryMessage = [];
 
-function showSuccess(input) {
-	const formControl = input.parentElement;
-	formControl.className = 'form-control success';
-	return false;
-}
+	function showError(input, msg) {
+		const formGroup = input.parentElement;
+		formGroup.className = 'form-group error';
+		arryMessage.push(msg);
+		message.textContent = [...new Set(arryMessage)]
+			.map((item, index) => {
+				return `${index + 1}. ${item
+					.charAt(0)
+					.toUpperCase()}${item.slice(1)}`;
+			})
+			.join('\n');
+		return false;
+	}
+	function showSuccess(inputArray) {
+		inputArray.forEach(input => {
+			const formGroup = input.parentElement;
+			formGroup.className = 'form-group success';
+			message.textContent = '';
+			arryMessage = [];
+		});
+	}
 
-function checkRequired(inputs) {
-	inputs.forEach(input => {
+	function checkUsername(input) {
 		if (input.value.trim() === '') {
-			showError(input, `please enter ${input.getAttribute('id')}`);
+			showError(input, `please enter ${input.id}`);
+		} else if (input.value.trim() !== nameUser) {
+			showError(input, `please enter a valid ${input.id}`);
+		} else {
+			showSuccess([input]);
+		}
+	}
+
+	function checkPassword(input) {
+		if (input.value.trim() === '') {
+			showError(input, `please enter ${input.id}`);
+		} else if (input.value.trim() !== passwordUser) {
+			showError(input, `please enter a valid ${input.id}`);
+		} else {
+			showSuccess([input]);
+		}
+	}
+
+	function succesfulLogin(
+		showSuccess,
+		username,
+		password,
+		arryMessage,
+		message,
+		myForm,
+	) {
+		showSuccess([username, password]);
+		let mesaj = 'Successful login!';
+		arryMessage.unshift(mesaj);
+		message.textContent = [...new Set(arryMessage)].splice(0, 1);
+		myForm.insertAdjacentElement('beforerend', message.textContent);
+	}
+
+	myForm.addEventListener('submit', e => {
+		e.preventDefault();
+
+		if (
+			username.value.trim() === nameUser &&
+			password.value.trim() === passwordUser
+		) {
+			succesfulLogin(
+				showSuccess,
+				username,
+				password,
+				arryMessage,
+				message,
+				myForm,
+			);
+		} else {
+			message.textContent = '';
+			arryMessage = [];
+			checkUsername(username);
+			checkPassword(password);
 		}
 	});
-}
-
-function checkUsername(input) {
-	if (input.value.trim() !== nameUser) {
-		showError(input, 'please enter a valid username');
-	}
-}
-
-function checkPassword(input) {
-	if (input.value.trim() !== passwordUser) {
-		showError(input, 'please enter a valid password');
-	}
-}
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	checkRequired([username, password]);
-	checkUsername(username);
-	checkPassword(password);
+	username.addEventListener('blur', () => checkUsername(username));
+	password.addEventListener('blur', () => checkPassword(password));
 });
