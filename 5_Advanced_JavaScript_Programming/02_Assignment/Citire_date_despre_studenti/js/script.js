c4ef4126f15a9a12069b4f4a0776b24b5719b85d
timi-petre <12413810+timi-petre@users.noreply.github.com>
2023-09-27 13:35:38 +0300

    Assignment 2 - Citire date studenti

--- 5_Advanced_JavaScript_Programming/02_Assignment/Citire_date_despre_studenti/js/script.js
+++ 5_Advanced_JavaScript_Programming/02_Assignment/Citire_date_despre_studenti/js/script.js
@@ -1,29 +1,59 @@
-const getStudents = document.getElementById('studenti')
+class Student {
+	constructor(name, address, phone, course) {
+		this.name = name
+		this.address = address
+		this.phone = phone
+		this.course = course
+	}
+
+	getInfo() {
+		return `
+		<h2>Name: ${this.name}</h2>
+		<p>Address: ${this.address}</p>
+		<p>Phone: ${this.phone}</p>
+		<p>Course: ${this.course}</p>
+`
+	}
+}
+
+const getStudents = document.getElementById('students')
 
 async function getData() {
 	try {
-		const response = await fetch(
+		let response = await fetch(
 			'https://v-dresevic.github.io/Advanced-JavaScript-Programming/data/students.txt',
 		)
 
 		if (response.status !== 200) {
 			throw new Error('Error reading data')
 		}
+		let text = await response.text()
+		let rows = text.split('\n')
 
-		const text = await response.text()
-		const rows = text.split('\n')
+		const s = []
+		for (var i = 0; i < rows.length; i += 4) {
+			s.push(rows.slice(i, i + 4))
+		}
 
-		const studentCards = rows
-			.reduce((acc, stud, index) => {
-				if (index % 4 === 0) {
-					const [name, address, phone, course] = rows.slice(index, index + 4)
-					const student = new Student(name, address, phone, course)
-					acc.push(student.getInfo())
-				}
-				return acc
-			}, [])
-			.join('')
+		let students = []
 
+		for (let i = 0; i < s.length; i++) {
+			let row = s[i]
+			let name = row[0]
+			let address = row[1]
+			let phone = row[2]
+			let course = row[3]
+			let student = new Student(name, address, phone, course)
+			students.push(student)
+		}
+
+		// Generate student cards
+		let studentCards = ''
+		for (const student of students) {
+			studentCards += student.getInfo()
+		}
+
+		// Set the generated student cards in the 'studenti' container
 		getStudents.innerHTML = studentCards
 	} catch (err) {
 		getStudents.innerHTML = 'Fetch problem: ' + err.message
