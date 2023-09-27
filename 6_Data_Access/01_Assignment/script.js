15a396bc618c5d4a0132addc1b0221fe869bb667
timi-petre <12413810+timi-petre@users.noreply.github.com>
2023-09-27 16:53:08 +0300

    (Directory) 01_Assignment-Lucrul_date

--- 6_Data_Access/01_Assignment/script.js
+++ 6_Data_Access/01_Assignment/script.js
@@ -1,37 +1,42 @@
 window.addEventListener('DOMContentLoaded', async () => {
 	try {
 		let response = await fetch('quiz.json')
+
 		if (!response.ok) {
 			throw new Error('Error while fetching data')
 		}
+
 		let data = await response.json()
 
-		questionsData(data)
+		displayQuestions(data)
 	} catch (error) {
 		console.log('Fetch problem: ' + error.message)
 	}
 })
 
-function questionsData(quizzes) {
+function displayQuestions(quizzes) {
 	const container = document.createElement('div')
+
 	const storedAnswers = JSON.parse(window.localStorage.getItem('answers')) || {}
 
 	for (const key in quizzes.quiz) {
 		const { question, options, answer } = quizzes.quiz[key]
 
 		const questionDiv = document.createElement('div')
+
 		questionDiv.innerHTML = `<h2><span>Question ${key.charAt(
 			1,
 		)}: </span>${question}</h2><br>
-		${options
+    ${options
 			.map(
 				item => `
-			<input type="radio" name="${key}" value="${item}" />
-			<label>${item}</label><br>
-		`,
+      <input type="radio" name="${key}" value="${item}" />
+      <label>${item}</label><br>
+    `,
 			)
 			.join('')}
-	`
+    `
+
 		const storedAnswer = storedAnswers[key]
 		if (storedAnswer) {
 			const radioInput = questionDiv.querySelector(`input[value="${storedAnswer}"]`)
@@ -42,18 +47,20 @@ function questionsData(quizzes) {
 
 		container.appendChild(questionDiv)
 	}
+
 	document.body.appendChild(container)
 
-	container.addEventListener('change', handleChange)
+	container.addEventListener('change', handleResponse)
 }
 
-function handleChange(event) {
+function handleResponse(event) {
 	if (event.target.type === 'radio') {
-		const q = event.target.name
+		const question = event.target.name
 		const answer = event.target.value
 
 		const storedAnswers = JSON.parse(window.localStorage.getItem('answers')) || {}
-		storedAnswers[q] = answer
+
+		storedAnswers[question] = answer
 
 		window.localStorage.setItem('answers', JSON.stringify(storedAnswers))
 	}
